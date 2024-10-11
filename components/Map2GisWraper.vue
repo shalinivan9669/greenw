54fdc2e0601641acf298f4cc01889c00140267f3
shalinivan9669 <77225504+shalinivan9669@users.noreply.github.com>
2024-10-11 22:15:23 +0600

    deleted  and map lazy

--- components/Map2GisWraper.vue
+++ components/Map2GisWraper.vue
@@ -1,15 +1,51 @@
 <template>
-  <div>
-    <Map2GIS />
+  <div id="mapContainer">
+    <div v-if="isMapVisible">
+      <Suspense>
+        <template #default>
+          <AsyncMap2GIS />
+        </template>
+        <template #fallback>
+          <div>Загрузка карты...</div>
+        </template>
+      </Suspense>
+    </div>
   </div>
 </template>
 
 <script>
-import Map2GIS from './Map2Gis.vue';
+import { defineAsyncComponent, onMounted, ref } from 'vue';
 
 export default {
   components: {
-    Map2GIS,
+    AsyncMap2GIS: defineAsyncComponent(() => import('./Map2Gis.vue')),
+  },
+  setup() {
+    const isMapVisible = ref(false);
+
+    // Функция для отслеживания видимости
+    const observeVisibility = (el) => {
+      const observer = new IntersectionObserver(
+        (entries) => {
+          if (entries[0].isIntersecting) {
+            isMapVisible.value = true;
+            observer.disconnect(); // Отключаем наблюдатель после загрузки карты
+          }
+        },
+        { threshold: 0.1 } // Настройка отслеживания - 10% элемента в области видимости
+      );
+      observer.observe(el);
+    };
+
+    // Используем onMounted, чтобы начать отслеживать компонент после монтирования
+    onMounted(() => {
+      const mapContainer = document.querySelector('#mapContainer');
+      if (mapContainer) {
+        observeVisibility(mapContainer);
+      }
+    });
+
+    return { isMapVisible };
   },
 };
 </script>
