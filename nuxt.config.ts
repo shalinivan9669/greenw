5ed7adc2919c508cab494da3f2b9f73a0a283170
shalinivan9669 <77225504+shalinivan9669@users.noreply.github.com>
2024-10-12 21:25:55 +0600

    img

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -100,7 +100,7 @@ export default defineNuxtConfig({
 
   // Настройки для модуля @nuxt/image
   image: {
-    dir: 'static',
+    dir: 'public',
     format: ['webp', 'png'],
     providers: {
       // Опциональные провайдеры изображений, например Cloudinary или imgix
